dd0210d248a276419bf9efb7950f12c1cdeb0dfe
GaussFighting <krzysztof.kowerczyk@gmail.com>
2026-09-26 06:08:56 +0200

    createPayment lambda connected to DynamoDB

--- lambdas/createPayment/index.js
+++ lambdas/createPayment/index.js
@@ -1,17 +1,31 @@
 import { randomUUID } from "crypto";
 
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+
+const docClient = DynamoDBDocumentClient.from(client);
+
 export const handler = async (event) => {
   try {
-    const body = JSON.parse(event.body);
+    console.log("EVENT:", JSON.stringify(event));
+    const body = event;
+    console.log("BODY:", JSON.stringify(body));
+    const { dueDate, flatName, notes, paymentType, value } = body;
 
-    const payment = {
-      uuid: randomUUID(),
-      dueDate: body.dueDate,
-      flatName: body.flatName,
-      notes: body.notes,
-      paymentType: body.paymentType,
-      value: body.value,
+    const params = {
+      TableName: "payments-develop",
+      Item: {
+        uuid: randomUUID(),
+        dueDate: `${dueDate}T00:00:00Z`,
+        flatName,
+        notes,
+        paymentType,
+        value,
+      },
     };
+    await docClient.send(new PutCommand(params));
 
     return {
       statusCode: 201,
@@ -20,18 +34,23 @@ export const handler = async (event) => {
         "Access-Control-Allow-Credentials": "true",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(payment),
+      body: JSON.stringify({
+        message: "Payment created successfully",
+        item: params.Item,
+      }),
     };
   } catch (error) {
+    console.error("DynamoDB error:", error);
+
     return {
-      statusCode: 400,
+      statusCode: 500,
       headers: {
         "Access-Control-Allow-Origin": "http://localhost:3000",
         "Access-Control-Allow-Credentials": "true",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        message: "Invalid request",
+        message: "Failed to create payment",
       }),
     };
   }
